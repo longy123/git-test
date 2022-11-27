4b313271c1ab6619aba97c4a5c55d70e46b5768f
Long <Long@atying.com>
2022-11-27 17:09:56 +0800

    2 commit

--- src/main/java/com/atguiu/git/GitTest.java
+++ src/main/java/com/atguiu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.atguiu.git;
 public class GitTest {
     public static void main(String[] args){
         System.out.println("hellow git");
+        System.out.println("hellow git");
     }
 }
