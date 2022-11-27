f10595fa66b84a4d6515f8101e80099b9e489134
longy123 <119065416+longy123@users.noreply.github.com>
2022-11-27 18:18:46 +0800

    pull commit

--- src/main/java/com/atguiu/git/GitTest.java
+++ src/main/java/com/atguiu/git/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("hellow githnjkndf1");
         System.out.println("hellow githnjkndf44");
         System.out.println("hellow githnjkndf5");
+         System.out.println("hellow githnjkndf12");
 
 
     }
